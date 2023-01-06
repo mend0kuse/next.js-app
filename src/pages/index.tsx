608fca8009c0ba677a9f6e888b471b6418694076
Васильев Семён <s.vasilev@vipservice.ru>
2023-01-06 22:29:01 +0800

    Добавляет запрос данных с API для меню и путей

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
+import axios from 'axios';
+import { MenuItem } from 'src/interfaces/menu';
 import { withLayout } from 'src/layout';
 
-import { useState } from 'react';
+import { GetStaticProps } from 'next';
+import { FC, useState } from 'react';
 
 import Button from '@components/UI/Button';
 import H from '@components/UI/HTag';
@@ -8,7 +11,12 @@ import P from '@components/UI/PTag';
 import Tag from '@components/UI/Tag';
 import StarRating from '@components/UI/star-rating';
 
-export function Home() {
+interface HomeProps extends Record<string, unknown> {
+	menu: MenuItem[];
+	firstCategory: number;
+}
+
+export const Home: FC<HomeProps> = ({ menu }) => {
 	const [rat, setRat] = useState(3);
 	return (
 		<>
@@ -19,8 +27,25 @@ export function Home() {
 			<P size='xl'>Lorem ipsum dolor sit, amet consectetur adipisicing.</P>
 			<Tag color='primary'>ФЫВфЫВ</Tag>
 			<StarRating rating={rat} setRating={setRat} />
+			<ul>
+				{menu.map((item) => {
+					return <li key={item._id.secondCategory}>{item._id.secondCategory}</li>;
+				})}
+			</ul>
 		</>
 	);
-}
+};
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps = async () => {
+	const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
+		firstCategory: 0,
+	});
+	return {
+		props: {
+			menu,
+			firstCategory: 0,
+		},
+	};
+};
